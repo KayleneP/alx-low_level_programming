b1e3256de06eaf69c92481f64fb2a08154b4af61
Kaylene <kayilpillay@gmail.com>
2022-06-15 05:18:01 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - Entry point
+ * Description: Main does some tings
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+fprintf(stderr, NC "\n");
+return (1); }
