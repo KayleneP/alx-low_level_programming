71347cbf3f131c275316c288a380c4e61f1a77b5
Kaylene <kayilpillay@gmail.com>
2022-06-15 04:37:21 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
 #include <string.h>
-/**
- * main - Entry point 
+/* main - Entry point 
  * Description: 'the program's description'
- * Return: Always 0 (Success)
- */
+ * Return: Always 0 (Success) */
 int main(void)
 {puts("\"Programming is like building a multilingual puzzle");
 return (0); }
