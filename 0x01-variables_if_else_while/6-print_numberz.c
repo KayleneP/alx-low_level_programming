538dabf342d0b3b1ef94b58ff8926da370f6b2f0
KayleneP <104757472+KayleneP@users.noreply.github.com>
2022-06-16 17:49:29 +0200

    Create 6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - Entry point
+ * Description: Main does some tings
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{int x;
+for (x = 48; x < 58; x++)
+{putchar(x); }
+putchar(10);
+return (0); }
