8e105c47735eb0293d4785e88378e6456441fda4
KayleneP <104757472+KayleneP@users.noreply.github.com>
2022-06-16 20:55:18 +0200

    Update 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,11 +11,11 @@
 int print_sign(int n)
 {
 if (n > 0)
-{putchar(43);
-return (1); }
+{putchar(43); }
 else if (n < 0)
 {putchar(45);
 return (-1); }
 else
 {putchar(48);
-return (0); }}
+return (0); }
+return (1); }
