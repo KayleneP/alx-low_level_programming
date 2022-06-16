270b90b8c67249b04245e1b4a0c32d55edcf37ae
KayleneP <104757472+KayleneP@users.noreply.github.com>
2022-06-16 17:54:48 +0200

    Update 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,1 +1,15 @@
-
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - Entry point
+ * Description: Main does some tings
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{int x;
+for (x = 48; x < 58; x++)
+{putchar(x); }
+for (x = 97; x < 103; x++)
+{putchar(x); }
+putchar(10);
+return (0); }
