d5dccb9c2a6ebe03beaab4d7f3bb9dcffa792504
KayleneP <104757472+KayleneP@users.noreply.github.com>
2022-06-16 20:06:27 +0200

    Update 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <string.h>
 /**
- * _islower - Entry point
- * Description: _islower does some tings
- * Return: c (Success)
+ * _islower - does some tings
+ * @c: The value to print
+ *
+ * Return: On success 1.
+ * On fail, 0 is returned.
  */
 int _islower(int c)
 {
