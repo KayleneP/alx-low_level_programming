8d77c38ac5f31a4440450240bba506b286c5319e
KayleneP <104757472+KayleneP@users.noreply.github.com>
2022-06-16 17:58:29 +0200

    Update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,1 +1,16 @@
-
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - Entry point
+ * Description: Main does some tings
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{int x;
+for (x = 48; x < 57; x++)
+{putchar(x); 
+putchar(44);
+putchar(32)
+}
+putchar(57);
+return (0); }
