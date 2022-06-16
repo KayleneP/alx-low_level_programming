4be1cb206f11d21f1ba6954e420cbd80c9788d5b
KayleneP <104757472+KayleneP@users.noreply.github.com>
2022-06-16 18:23:47 +0200

    Update 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,1 +1,22 @@
-
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - Entry point
+ * Description: Main does some tings
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{int x, y, z;
+for (x = 48; x < 56; x++)
+{
+for (y = x + 1; y < 57; y++)
+{
+for (z = y + 1; z < 58; z++)
+{putchar(x);
+putchar(y);
+putchar(z);
+if (x < 56 && y < 57 && z < 57)
+{putchar(44);
+putchar(32); }}}}
+putchar(10);
+return (0); }
