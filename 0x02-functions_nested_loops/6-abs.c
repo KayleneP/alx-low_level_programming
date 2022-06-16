f88cd5dc1f5c23cb4c01fde9ec4f23fc0686c821
KayleneP <104757472+KayleneP@users.noreply.github.com>
2022-06-16 21:11:59 +0200

    Update 6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,1 +1,15 @@
-
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+/**
+ * _abs - does some tings
+ * @n: The value to print
+ *
+ * Return: On success 1.
+ * On fail, 0 is returned.
+ * On something, -1
+ */
+int _abs(int n)
+{
+if (n < 0)
+{n *= -1; }}
