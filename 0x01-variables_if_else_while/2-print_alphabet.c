0414d83cb5cc1799f73cf7f7fbbc8eb360dbcb2a
KayleneP <104757472+KayleneP@users.noreply.github.com>
2022-06-16 17:32:32 +0200

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,11 +6,8 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{
-int x;
-for(x=97; x<123; x++){
-putchar(x);
-}
+{int x;
+for(x=97; x<123; x++)
+{putchar(x); }
 putchar(4);
-return (0);
-}
+return (0); }
