caf5294b8563a81f388d3c7eaedcca90278ef9ce
KayleneP <104757472+KayleneP@users.noreply.github.com>
2022-06-16 17:08:38 +0200

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,23 +1,10 @@
 #include <stdio.h>
-#include <string.h>
-/**
- * main - Entry point
- * Description: Main does some tings
- * Return: Always 0 (Success)
- */
-int main(void)
-{#include < stdio.h >
-
-int main()
-{
-    char ch = 'a';
-
-    printf("Lowercase English Alphabets:\n");
-    while(ch <= 'z')
-    {
-        printf("%c ", ch);
-        ch++;
-    }
-    printf("\n");
-  
-return (0); }
+ #include <string.h>
+ /**
+  * main - Entry point
+  * Description: Main does some tings
+  * Return: Always 0 (Success)
+  */
+ int main(void)
+ {printf("with proper grammar, but the outcome is a piece of art,\n");
+ return (0); }
