47e4f5194c3c02a21ffc7cf7b7cc94a4208fec04
KayleneP <104757472+KayleneP@users.noreply.github.com>
2022-06-16 18:26:31 +0200

    Update 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -15,7 +15,7 @@ for (z = x + 2; z < 58; z++)
 {putchar(x);
 putchar(y);
 putchar(z);
-if (x < 56 && y < 57 && z < 57)
+if (x < 55 && y < 57 && z < 58)
 {putchar(44);
 putchar(32); }}}}
 putchar(10);
