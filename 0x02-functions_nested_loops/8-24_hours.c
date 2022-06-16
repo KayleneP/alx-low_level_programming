d5c87bc066e639ea73cf3134892e004f2209af1a
KayleneP <104757472+KayleneP@users.noreply.github.com>
2022-06-16 22:01:36 +0200

    Update 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,1 +1,31 @@
-
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+/**
+ * jack_bauer - does some tings
+ * @n: The value to print
+ *
+ * Return: On success 1.
+ * On fail, 0 is returned.
+ * On something, -1
+ */
+void jack_bauer(void);
+{
+int a, b, c, d;
+for (a = 48; x <51; a++)
+{
+if (x == 50)
+{val = 52; }
+else
+{val = 58 ; }
+for (b = 48; y < val; b++)
+{
+for (c = 48; x <54; c++)
+{
+for (d = 48; y < 58; d++)
+{_putchar(a);
+ _putchar(b);
+ _putchar(58);
+ _putchar(c);
+ _putchar(d);
+ _putchar(10); }}}}
