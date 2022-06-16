70337d05df5e4af3837736e2c9d09f68cf7c1df0
KayleneP <104757472+KayleneP@users.noreply.github.com>
2022-06-16 18:12:21 +0200

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,1 +1,18 @@
-
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - Entry point
+ * Description: Main does some tings
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{int x, y;
+for (x = 48; x < 58; x++)
+{putchar(x);
+for (y = x-1, y <58; y++)
+{putchar(y);
+if (x < 57 && y < 57)
+{putchar(44);
+putchar(32); }}}
+putchar(10);
+return (0); }
