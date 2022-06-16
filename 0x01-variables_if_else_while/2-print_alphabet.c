649e91a6b95d6e72bd2a968ff145342c14ef261c
KayleneP <104757472+KayleneP@users.noreply.github.com>
2022-06-16 17:15:01 +0200

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,16 +6,12 @@
  * Return: Always 0 (Success)
  */
 int main()  
-{  
-    char ch = 'a';  
-  
-    printf("Lowercase English Alphabets:\n");  
-    while(ch <= 'z')  
-    {  
-        printf("%c ", ch);  
-        ch++;  
-    }  
-    printf("\n");  
-  
-    return 0;  
+{char ch = 'a';
+printf("Lowercase English Alphabets:\n");
+while(ch <= 'z')
+{printf("%c ", ch);
+ch++;
+}
+printf("\n");
+return 0;
 }
