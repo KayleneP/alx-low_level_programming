8e5da28fe30ca8be0d25386ac47106bca8a644c5
KayleneP <104757472+KayleneP@users.noreply.github.com>
2022-06-16 18:16:24 +0200

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,9 +8,10 @@
 int main(void)
 {int x, y;
 for (x = 48; x < 57; x++)
-{putchar(x);
+{
 for (y = x - 1; y < 58; y++)
-{putchar(y);
+{putchar(x);
+putchar(y);
 if (x < 57 && y < 57)
 {putchar(44);
 putchar(32); }}}
