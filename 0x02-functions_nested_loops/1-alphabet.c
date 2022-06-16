52cb14c9cc1432aa92ccae10ab1246c6b6762af9
KayleneP <104757472+KayleneP@users.noreply.github.com>
2022-06-16 19:50:49 +0200

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,9 +5,8 @@
  * Description: Main does some tings
  * Return: Always 0 (Success)
  */
-int main(void)
+void print_alphabet(void);
 {int x;
 for (x = 97; x < 123; x++)
 {putchar(x); }
-putchar(10);
-return (0); }
+putchar(10); }
