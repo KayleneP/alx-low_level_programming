6a00bc9e247c23bb7328e1f51af1694173f05efb
KayleneP <104757472+KayleneP@users.noreply.github.com>
2022-06-16 20:01:00 +0200

    Update 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,1 +1,13 @@
-
+#include <stdio.h>
+#include <string.h>
+/**
+ * _islower - Entry point
+ * Description: _islower does some tings
+ * Return: void (Success)
+ */
+int _islower(int c);
+{if (c > 96)
+{return (1); }
+else
+{return (0); }
+}
