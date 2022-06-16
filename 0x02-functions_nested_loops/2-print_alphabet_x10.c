a6a0764a5e8a4daefe1a6583bce47a4d99a07eee
KayleneP <104757472+KayleneP@users.noreply.github.com>
2022-06-16 19:55:09 +0200

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,1 +1,14 @@
-
+#include <stdio.h>
+#include <string.h>
+/**
+ * print_alphabet - Entry point
+ * Description: print_alphabet does some tings
+ * Return: void (Success)
+ */
+void print_alphabet_x10(void)
+{int x, y;
+for (y = 0 ; y < 10; y++)
+{
+for (x = 97; x < 123; x++)
+{putchar(x); }}
+putchar(10); }
