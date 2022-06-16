5f06b7f1c3c958ca98675cf24e2153f1e2a13ba2
KayleneP <104757472+KayleneP@users.noreply.github.com>
2022-06-16 19:35:26 +0200

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,1 +1,11 @@
-
+#include <stdio.h>
+#include <string.h>
+#include <_putchar.h>
+/**
+ * main - Entry point
+ * Description: Main does some tings
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{char *_strcat(char *dest, char *src);
+return (0); }
