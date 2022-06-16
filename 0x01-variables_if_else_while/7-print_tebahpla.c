af5749b7e0e1f0ce5c0d26213f19b6f1c88dc38f
KayleneP <104757472+KayleneP@users.noreply.github.com>
2022-06-16 17:51:00 +0200

    Update 7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,1 +1,13 @@
-
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - Entry point
+ * Description: Main does some tings
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{int x;
+for (x = 122; x > 96; x--)
+{putchar(x); }
+putchar(10);
+return (0); }
