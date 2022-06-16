8da78d5f1fd8a24c107e64d13b3a3428fd9ada65
KayleneP <104757472+KayleneP@users.noreply.github.com>
2022-06-16 20:10:05 +0200

    Update 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,1 +1,16 @@
-
+#include <stdio.h>
+#include <string.h>
+/**
+ * isalpha - does some tings
+ * @c: The value to print
+ *
+ * Return: On success 1.
+ * On fail, 0 is returned.
+ */
+int _isalpha(int c)
+{
+if ((c > 64 && c < 91) || (c > 96 && c < 123))
+{return (1); }
+else
+{return (0); }
+}
