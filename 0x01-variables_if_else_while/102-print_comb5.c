1057775fae38ba2a8f02e9a157e8db74219000b1
KayleneP <104757472+KayleneP@users.noreply.github.com>
2022-06-16 18:35:42 +0200

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,1 +1,26 @@
-
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - Entry point
+ * Description: Main does some tings
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{int x, y, a, b;
+for (x = 48; x < 57; x++)
+{
+for (y = x; y < 58; y++)
+{
+for (x = 48; x < 57; x++)
+{
+for (y = x + 1; y < 58; y++)
+{putchar(x);
+putchar(y);
+putchar(32);
+putchar(a);
+putchar(b);
+if (x < 58 && y < 56 && a < 58 && b < 58)
+{putchar(44);
+putchar(32); }}}
+putchar(10);
+return (0); }
