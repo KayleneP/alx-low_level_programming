26c0d4ff1a04fa8c90421d0046484f4d9bb0d5e3
KayleneP <104757472+KayleneP@users.noreply.github.com>
2022-06-17 14:26:33 +0200

    Update 10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,1 +1,16 @@
-
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+/**
+ * add - does some tings
+ * @a: The value to print
+ * @b: The value to print
+ * Return: On success c.
+ * On fail, 0 is returned.
+ * On something, -1
+ */
+int add(int a, int b);
+{
+int c;
+c = a + b;
+return(c); }
