85971ab032f9a017012824d95967db21e83230a8
KayleneP <104757472+KayleneP@users.noreply.github.com>
2022-06-17 13:30:23 +0200

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,1 +1,21 @@
-
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+/**
+ * jack_bauer - does some tings
+ * Return: On success 1.
+ * On fail, 0 is returned.
+ * On something, -1
+ */
+void jack_bauer(void)
+{
+int a, b;
+for (a = 48; a < 51; a++)
+{
+for (b = 48; b < val; b++)
+{char c=(a*b)+'0';
+_putchar(c);
+_putchar(44);
+_putchar(32);
+_putchar(32);}
+_putchar(10);}}
